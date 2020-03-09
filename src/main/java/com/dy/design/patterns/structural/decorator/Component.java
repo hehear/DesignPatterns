d245adf6832e8cc97aae2f62b0c2ba94fdcb4254
dengxiangying <hehear@126.com>
2020-03-09 13:48:43 +0800

    装饰模式

---
+++ src/main/java/com/dy/design/patterns/structural/decorator/Component.java
@@ -0,0 +1,14 @@
+package com.dy.design.patterns.structural.decorator;
+
+/**
+ * @description Component是定义一个对象抽象类，可以给这些对象动态地添加职责
+ * @author dxy
+ * @date 20200309
+ */
+public abstract class Component {
+
+    /**
+     * 操作方法
+     */
+    public abstract void operation();
+}
